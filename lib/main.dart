70fd5c80fb9069a0024de29783addb6c11473f79
Pocholo <locsin.pochololem@gmail.com>
2023-04-20 23:51:20 +0800

    New starting code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyFirstApp());
-}
-
-class MyFirstApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text('Welcome!'),
-    );
-  }
+  runApp(MaterialApp(home: Text('Welcome')));
 }
